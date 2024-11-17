ec173d5f6c6037f7634e1537c9d9fca0cf4090da
vitor.gabriel <vitor.gabriel@sou.unifeob.edu.br>
2024-11-17 12:00:13 -0300

    [style] layout profile

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,27 +1,48 @@
 <template>
     <div class="profile">
-        <h2>Editar Perfil</h2>
+        <h1 class="title">Perfil</h1>
+        <div class="content-profile">
+            <img class="user-avatar" :src="userLocal.avatar" alt="avatar do usuário">
+            <div class="user-name-email">
+                <span>
+                    <strong>
+                        {{ userLocal.name }}
+                    </strong> 
+                </span>
+                <span>
+                    <strong>
+                        {{ userLocal.email }}
+                    </strong> 
+                </span>
+            </div>
+        </div>
+
+        <hr>
+        <h2 class="title">Editar Perfil</h2>
         <form @submit.prevent="updateProfile">
             <div>
                 <label for="name">Nome:</label>
-                <input id="name" v-model="userLocal.name" type="text" required />
+                <input class="input is-normal" id="name" v-model="userLocal.name" type="text" required />
             </div>
             <div>
                 <label for="email">Email:</label>
-                <input id="email" v-model="userLocal.email" type="email" required />
+                <input class="input is-normal" id="email" v-model="userLocal.email" type="email" />
             </div>
             <div>
                 <label for="avatar">Avatar URL:</label>
-                <input id="avatar" v-model="userLocal.avatar" type="url" required />
+                <input class="input is-normal" id="avatar" v-model="userLocal.avatar" type="url" required />
             </div>
-            <button type="submit">Salvar</button>
+            <button class="button is-success" type="submit">Salvar</button>
         </form>
     </div>
 </template>
 
 <script>
-    import { defineComponent } from 'vue';
-    import { user, setUser } from '@/utils/user';
+import { defineComponent } from 'vue';
+import { user, setUser } from '@/utils/user';
+import { typeNotification } from '@/interfaces/INotificacao';
+import useNotification from '@/hooks/notificador';
+
 
 export default defineComponent({
     name: 'Profile',
@@ -34,12 +55,18 @@ export default defineComponent({
             },
         };
     },
+    setup() {
+        const { notificar } = useNotification();
+        return {
+            notificar
+        };
+    },
     mounted() {
         this.userLocal = user;
     },
     methods: {
         updateProfile() {
-            alert('Perfil atualizado com sucesso!');
+            this.notificar('Parabéns!', 'Perfil atualizado com sucesso!', typeNotification.SUCCESS);
             setUser(this.userLocal);
         },
     },
@@ -50,8 +77,7 @@ export default defineComponent({
 
 <style scoped>
     .profile {
-        max-width: 400px;
-        margin: auto;
+        padding: 1.5rem;
     }
 
     label {
@@ -67,5 +93,29 @@ export default defineComponent({
 
     button {
         padding: 10px 20px;
+    }
+
+    .title {
+        font-size: 20pt;
+        margin-bottom: 1rem;
+    }
+
+    .content-profile {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .user-avatar {
+        width: 100%;
+        max-width: 130px;
+            
+        border-radius: 50%;
+        border: 5px solid #0D3B66;
+    }
+    .user-name-email span strong{
+        color: #0D3B66;
+        font-size: 16pt;
     }
 </style>
